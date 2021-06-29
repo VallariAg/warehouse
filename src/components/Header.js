891e687a3b92c5d3a3ea0241b2532edebc1ce5df
VallariAg <val.agl002@gmail.com>
2021-06-30 02:10:11 +0530

    edit responsive alignment in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,58 +1,59 @@
 import { ChevronDownIcon, SearchIcon } from "@heroicons/react/outline"
 
-
+function LoginOrUser({user}) {
+  if (!user) {
+    return (
+      <div className="flex items-center justify-end flex-1 lg:w-0">
+        <div class="rounded-full text-gray-600 text-md justify-self-center my-2 h-12 w-12 flex items-center justify-center bg-blue-100">V</div>
+        <ChevronDownIcon
+                          className="ml-2 h-5 w-5 text-gray-500 group-hover:text-gray-500"
+                          aria-hidden="true"
+            />
+      </div>
+    )
+  }
+  return (
+          <div className="md:flex items-center justify-end md:flex-1 lg:w-0">
+            {/* <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+              Sign in
+            </a> */}
+            <a href="#"
+              className="ml-8  whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+              Sign in
+            </a>
+          </div>
+  )
+}
 export default function Header() {
   return (
     <div className="App">
-       {/* <div className="max-w-7xl mx-auto px-4 sm:px-6"> */}
-        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-   
-          <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
+        <div className="flex justify-between items-center border-b-2 border-gray-100 py-4 md:justify-start md:space-x-10">
+          <a href="#" className="px-2 text-base font-medium text-gray-500 hover:text-gray-900">
             Logo
           </a>
-          <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
-            <div className="group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            Home
-            <ChevronDownIcon
-                          className="ml-2 h-5 w-5 group-hover:text-gray-500"
-                          aria-hidden="true"
-            />
-            </div>
+          <a href="#" className="px-2 text-base font-medium text-gray-500 hover:text-gray-900">
+              Home
           </a>
           
-          <div className="text-base font-medium text-gray-500 hover:text-gray-900">
-            <div className="group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <div className="m:flex m:items-center m:justify-end text-base font-medium text-gray-500 hover:text-gray-900">
+            <div className="">
               <SearchIcon
                             className="ml-2 h-5 w-5 group-hover:text-gray-500"
                             aria-hidden="true"
               />
-              <input
+              {/* <input
               type="text"
               name="price"
               id="price"
-              className="focus:ring-indigo-500 focus:border-indigo-500 block w-full px-8 py-2 pl-7 pr-12 sm:text-sm border-gray-100 rounded-md"
+              className="focus:ring-indigo-500 focus:border-indigo-500 sm:w-0 block w-full px-8 py-2 pl-7 pr-12 border-gray-100 rounded-md"
               placeholder="search for items"
-            />
+            /> */}
             </div>
           </div>
-
-          <div className="mt-1 relative rounded-md shadow-sm">
-            
-          </div>
       
-
-          <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-            <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
-              Sign in
-            </a>
-            <a href="#"
-              className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-            >
-              Sign up
-            </a>
-          </div>
+          <LoginOrUser user="" />
+          
         </div>
-      {/* </div> */}
     </div>
   );
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,7 +4,7 @@ function Card() {
     return (
         <a href="#" className="mx-1 my-0 bg-gray-50 p-1 rounded-lg ">
           <img className="w-full rounded-md" src={cardDefaultImg}></img>
-          <div className="relative bg-gray-50 sm:p-2 p-2 w-full">
+          <div className="relative bg-gray-50 sm:p-3 p-3 w-full">
             {/* <div className="px-1"> */}
               <span className="mt-1 text-lg leading-tight font-medium text-gray-700"> Some long  long Title </span>
               <span className="text-gray-500 right-0 float-right"> 5 links </span>
@@ -17,7 +17,7 @@ function Card() {
 function ProfileHeader() {
   return(
     <div className="grid justify-center my-5">
-      <div class="rounded-full text-gray-600 text-5xl justify-self-center my-2 h-32 w-32 flex items-center justify-center bg-profile-bg">V</div>
+      <div class="rounded-full text-gray-600 text-5xl justify-self-center my-2 h-32 w-32 flex items-center justify-center bg-blue-50">V</div>
       <h1 className="text-2xl justify-self-center">Vallari Agrawal</h1>
       <h3 className="text-gray-700 text-md justify-self-center">@username</h3>
       <h3 className="text-gray-700 text-lg justify-self-center">
