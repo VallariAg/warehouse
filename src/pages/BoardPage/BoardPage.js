5458776ca637be065a2b2f56c88faced40ba0210
VallariAg <val.agl002@gmail.com>
2021-07-01 02:51:28 +0530

    Add edit-board icon in /board route
    - make empty files for popups in board page

--- src/pages/BoardPage/BoardPage.js
+++ src/pages/BoardPage/BoardPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { PlusCircleIcon } from "@heroicons/react/outline";
+import { PlusCircleIcon, PencilIcon } from "@heroicons/react/outline";
 import Card from "./PostCard";
 
 
@@ -9,8 +9,11 @@ export default function BoardPage() {
       <div className="">
         <h1 className="text-4xl text-gray-700 my-5 mx-3 lg:my-8 md:my-5 sm:my-5">
             Board Title
+            <button className="mx-3">
+                <PencilIcon className="ml-2 h-5 w-5 text-gray-500 hover:text-gray-800" /> 
+            </button>
             <button className="right-0 float-right">
-                <PlusCircleIcon className="ml-2 h-9 w-9 text-gray-500 hover:text-gray-800" /> 
+                <PlusCircleIcon className="ml-2 h-9 w-9 text-gray-500 hover:text-gray-800" />
             </button>
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 sm:grid-cols-1 my-2">
